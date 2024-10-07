9687a804ffdc9673fade39d965cd535143415cd6
Levon7878 <lyovbabagulyan@gmail.com>
2024-10-07 10:10:59 +0400

    product popup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
