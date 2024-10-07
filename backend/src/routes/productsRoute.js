e499e08fef1fdb5c4a050aed079dcb08eac99690
DavChaxalyan <dchaxalyan@gmail.com>
2024-10-07 18:54:20 +0400

    add product logic

--- backend/src/routes/productsRoute.js
+++ backend/src/routes/productsRoute.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const authController = require('../controllers/authController');
 const productController = require('../controllers/productController');
 const upload = require('../middleware/multer');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
-// Маршруты для регистрации и логина
-router.post('/register', authController.register);
-router.post('/login', authController.login);
 router.post('/add', authMiddleware, upload.single('image'), productController.addProduct);
 
 module.exports = router;
