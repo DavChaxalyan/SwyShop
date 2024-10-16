93944c7871d360fba6946172c918855796331c5a
DavChaxalyan <dchaxalyan@gmail.com>
2024-10-16 18:40:57 +0400

    fix bugs

--- frontend/src/Utils/utils.js
+++ frontend/src/Utils/utils.js
@@ -1,3 +1,5 @@
+import { jwtDecode } from "jwt-decode";
+
 export const getBase64 = (file) => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -6,3 +8,18 @@ export const getBase64 = (file) => {
         reader.onerror = (error) => reject(error);
     });
 };
+
+export const getUserIdFromToken = () => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        return decodedToken.userId;
+      } catch (error) {
+        console.error("Error decoding token:", error);
+        return null;
+      }
+    }
+    return null;
+  };
